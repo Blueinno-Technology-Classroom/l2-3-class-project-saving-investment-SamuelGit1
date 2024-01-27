78f6deba9e877b5a24b20d59c8ee02541f1fb36b
Samuel <wongsamuel12321@gmail.com>
2024-01-27 15:00:01 +0800

    submit

--- main.py
+++ main.py
@@ -1,43 +1,23 @@
 ##################################################
 '''
-Q1: 
+Challenge
 '''
 
-# TODO: Write your code here
+def round_off(num):
+    return float("{:.2f}".format(num))
 
-##################################################
-'''
-Q2:
-'''
+def investment(initial_investment, deposit_every_year):
+    current_balance = round_off(initial_investment)
+    deposit = round_off(deposit_every_year)
+    interest_per_year = 0.065
+    for i in range(1, 26):
+        if i <= 9:
+            year = '0' + str(i)
+        else :
+            year = str(i)
+        interest = round_off(current_balance * interest_per_year)
+        new_balance = round_off(current_balance + interest)
+        print(f'{year}: current balance: {current_balance}, interest: {interest},  deposit: {deposit}, new balance: {new_balance}')
+        current_balance = new_balance
 
-# TODO: Write your code here
-
-##################################################
-'''
-Q3:
-'''
-
-# TODO: Write your code here
-
-##################################################
-'''
-Q4:
-'''
-
-# TODO: Write your code here
-
-##################################################
-'''
-Q5:
-'''
-
-# TODO: Write your code here
-
-##################################################
-'''
-Q6:
-'''
-
-# TODO: Write your code here
-
-##################################################
+investment(1000, 100)
